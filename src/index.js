dc5f4fa794021d1ab68d06c940ebae122b3fe7eb
dacicus75 <dacicus75@hotmail.com>
2019-10-17 21:50:12 -0400

    moved colors ans sizes to variables.js

--- src/index.js
+++ src/index.js
@@ -5,47 +5,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import styled, { createGlobalStyle } from 'styled-components'
 
+// Import colors and sizes variables
+import { colors, sizes } from './variables'
+
 // Import style guide components
 // import Buttons from './components/buttons'
 // import Colors from './components/colors'
 // import Forms from './components/forms'
 // import Typography from './components/typography'
 
-// Codes for color palette
-const colors = {
-  disabled: 'hsl(212.3, 16.7%, 69.4%)',
-  error: 'hsl(359.6, 82.1%, 62.7%)',
-  errorActive: 'hsl(359.6, 82.1%, 42.7%)',
-  errorHover: 'hsl(359.6, 82.1%, 65%)',
-  primary: 'hsl(209.6, 100%, 55.9%)',
-  primaryActive: 'hsl(209.6, 100%, 35.9%)',
-  primaryHover: 'hsl(209.6, 100%, 65%)',
-  secondary: 'hsl(29.4, 100%, 63.1%)',
-  secondaryActive: 'hsl(29.4, 100%, 43.1%)',
-  secondaryHover: 'hsl(29.4, 100%, 65%)',
-  success: 'hsl(164, 75.6%, 46.7%)',
-  successActive: 'hsl(164, 75.6%, 26.7%)',
-  successHover: 'hsl(164, 75.6%, 60%)',
-  text: 'hsl(223.8, 81.3%, 6.3%)'
-}
-
-// Sizes for typography scale
-const typography = {
-  xs: '12px',
-  sm: '14px',
-  base: '16px',
-  lg: '18px',
-  xl: '20px',
-  xxl: '24px',
-  xxxl: '30px',
-  xxxxl: '36px'
-}
-
 // Global styles and resets
 createGlobalStyle`
   html {
     box-sizing: border-box;
-    font-size: ${typography.base};
+    font-size: ${sizes.base};
   }
 
   *,
@@ -79,7 +52,6 @@ const StyleguideHeading = styled.h1`
   display: inline-block;
   font-weight: 500;
 
-  // Customizable underline
   &::before {
     position: absolute;
     bottom: 0;
@@ -99,7 +71,6 @@ const StyleguideSubheading = styled.h2`
   font-weight: 400;
   text-align: left;
 
-  // Customizable underline
   &::before {
     position: absolute;
     bottom: 0;
@@ -127,19 +98,19 @@ class App extends React.Component {
 
         <StyleguideSubheading>Colors</StyleguideSubheading>
 
-        {/*<Colors colors={colors} />*/}
+        <Colors />
 
         <StyleguideSubheading>Typography</StyleguideSubheading>
 
-        {/*<Typography colors={colors} scale={typography} />*/}
+        <Typography />
 
         <StyleguideSubheading>Buttons</StyleguideSubheading>
 
-        {/*<Buttons colors={colors} />*/}
+        <Buttons />
 
         <StyleguideSubheading>Forms</StyleguideSubheading>
 
-        {/*<Forms colors={colors} scale={typography} />*/}
+        <Forms />
       </AppContainer>
     )
   }
