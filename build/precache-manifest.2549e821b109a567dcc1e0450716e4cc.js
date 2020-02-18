e3a028e163a4e3d04a460aaeded7574b92529710
dacicus75 <dacicus75@hotmail.com>
2020-02-17 21:14:45 -0500

    ran production build

---
+++ build/precache-manifest.2549e821b109a567dcc1e0450716e4cc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d89e37110051c21d3225b3f6ac32de7",
+    "url": "/dacicus75/UI_Style_Guide/index.html"
+  },
+  {
+    "revision": "74b6b6ebc650a9b89d8f",
+    "url": "/dacicus75/UI_Style_Guide/static/js/2.7e79101a.chunk.js"
+  },
+  {
+    "revision": "07094a4f18e91ccb81cf",
+    "url": "/dacicus75/UI_Style_Guide/static/js/main.ba52e7d1.chunk.js"
+  },
+  {
+    "revision": "36b1d8333f7f14770687",
+    "url": "/dacicus75/UI_Style_Guide/static/js/runtime-main.acdcdac1.js"
+  }
+]);
